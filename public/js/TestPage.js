3314af2e582f89f692446f69f47555da5c8cfaa9
hijazi (mac m1ni) <hijazi@irekasoft.com>
2021-04-27 15:22:22 +0800

    make no change when hover on selected item

--- public/js/TestPage.js
+++ public/js/TestPage.js
@@ -32183,7 +32183,7 @@ if (document.getElementById("root")) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/hijazi/Laravel/ir_laravel_boilerplate/resources/react/pages/TestPage.js */"./resources/react/pages/TestPage.js");
+module.exports = __webpack_require__(/*! /Users/hijazi/Laravel/mvp/ir_laravel_boilerplate/resources/react/pages/TestPage.js */"./resources/react/pages/TestPage.js");
 
 
 /***/ })
