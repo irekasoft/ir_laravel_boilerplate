05794bcb08b36b4330153eefd36c1862e570f50c
hijazi (mac studio) <hijazi@irekasoft.com>
2024-01-12 17:37:02 +0800

    updated to laravel 8.xx

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,15 +2,16 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Illuminate\Http\JsonResponse;
 
 class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
@@ -19,78 +20,42 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
 
     /**
-     * Report or log an exception.
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function register()
     {
-        parent::report($exception);
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
-
-        if (strpos($request->getUri(), '/api/') !== false) {
-
-            if ($exception instanceof HttpException) {
-                $response['message'] = Response::$statusTexts[$exception->getStatusCode()];
-                $response['status_code'] = $exception->getStatusCode();
-            } else if ($exception instanceof ModelNotFoundException) {
-                $response['message'] = Response::$statusTexts[Response::HTTP_NOT_FOUND];
-                $response['status_code'] = Response::HTTP_NOT_FOUND;
-            }
-
-            $response = [
-                'msg' => (string)$exception,
-                'status_code' => '404',
-            ];
-
-            return \Response::json($response)->setStatusCode(404);
-
-            if ($this->isDebugMode()) {
-                $response['debug'] = [
-                    'exception' => get_class($exception),
-                    'trace' => $exception->getTrace()
-                ];
-            }
-
-            return response()->json([
-            'status'      => 'failed',
-            'status_code' => $response['status_code'],
-            'msg'     => $response['message'],
-            ], $response['status_code']);
-
-        } else if ($this->isHttpException($exception)) {
-
-            if ($exception->getStatusCode() == 404) {
-
-                return response()->view('errors.404', [], 404);
-
-            }else if ($exception->getStatusCode() == 500){
-
-                return response()->view('errors.500', [], 500);
-
-            }
+        if ($request->expectsJson() || $request->is('api/*')) {
+            return $this->handleApiException($request, $exception);
         }
 
         return parent::render($request, $exception);
+    }
+
+    private function handleApiException($request, Throwable $exception)
+    {
+        $response = [
+            'error' => $exception->getMessage(),
+        ];
+
+        return new JsonResponse($response, 500);
     }
 
 }
